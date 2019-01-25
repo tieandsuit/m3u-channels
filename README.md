fe22c43cb06c5615f3b819b0bc20e20d83c619c4
tieandsuit <43476731+tieandsuit@users.noreply.github.com>
2019-01-25 11:57:49 +0100

    with player

---
+++ README.md
@@ -0,0 +1,6 @@
+# u3m8-player
+
+test at
+
+
+https://tieandsuit.github.io/m3u-channels/
--- index.php
+++
@@ -1,5 +0,0 @@
-<?php
-header("Access-Control-Allow-Origin: *");
-$playlist = file_get_contents('playlist.m3u');
-echo($playlist);
-?>
